f70f34223574b2ff72ca2a6539539a30ee64f3e4
欧阳斌 <ouyangbin81076@gmail.com>
2018-10-03 07:42:12 +0800

    add sourcemaps

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,14 +4,17 @@
 import * as del from "del";
 import * as gulp from "gulp";
 import * as jeditor from "gulp-json-editor";
+import * as sourcemaps from "gulp-sourcemaps";
 import * as ts from "gulp-typescript";
 
 const tsProject = ts.createProject("tsconfig.prod.json");
 // task for typescript
 gulp.task("ts", () => {
   return tsProject.src()
+    .pipe(sourcemaps.init())
     .pipe(tsProject())
-    .js.pipe(gulp.dest("build"));
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("build"));
 });
 const editPackageJson = jeditor((pkg: any) => {
   delete pkg.devDependencies;
@@ -28,9 +31,16 @@ gulp.task("package.json", () => {
     .pipe(gulp.dest("./build"));
 });
 
-gulp.task("clean", () => del("build"));
+gulp.task("clean:build", () => del("build"));
+gulp.task("clean", () => del([
+  "node_modules",
+  "build",
+  "**/node_modules",
+  "*.log",
+  "**/{lib,build,dist}",
+]));
 const build = gulp.series(
-  "clean",
+  "clean:build",
   gulp.parallel("package.json", "ts"),
 );
 gulp.task("default", build);
